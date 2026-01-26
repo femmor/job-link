bcb59eb98c15a0696874f91f1e95494efadd7f81
Emmanuel Egomson <fegomson@gmail.com>
2026-01-26 16:13:35 -0600

    feat: add models for Board, Column, and JobApplication with schemas and interfaces

---
+++ lib/models/column.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+
+export interface IColumn extends Document {
+    name: string;
+    boardId: mongoose.Types.ObjectId;
+    order: number;
+    jobApplications: mongoose.Types.ObjectId[];
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const ColumnSchema: Schema = new Schema<IColumn>(
+    {
+        name: { type: String, required: true },
+        boardId: { type: Schema.Types.ObjectId, ref: "Board", required: true, index: true },
+        order: { type: Number, required: true },
+        jobApplications: [{ type: Schema.Types.ObjectId, ref: "JobApplication" }],
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const Column = mongoose.models.Column || mongoose.model<IColumn>("Column", ColumnSchema);
